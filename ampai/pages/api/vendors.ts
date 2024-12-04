e56c5d06c8c872665aa32f68adc8b106d1119f50
chriswang06 <chrismcwang1@gmail.com>
2024-12-05 01:10:45 +0200

    added case insensitivity(regexp)

--- ampai/pages/api/vendors.ts
+++ ampai/pages/api/vendors.ts
@@ -9,10 +9,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     
     try {
         console.log('Search query:', search);
+        let reg = new RegExp(search, "i");
 
         const vendors = await db
             .collection('vendors')   
-            .find({"vendor_name": {$regex : search}})
+            .find({"vendor_name": {$regex : reg}})
             .toArray();
 
         res.status(200).json({ vendors });
